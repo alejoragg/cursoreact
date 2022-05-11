2555d506de0a70ce58ed56ecf45c42e84b3c4d71
Ale Aranguren <alejoragg@gmail.com>
2022-05-11 02:23:47 -0500

    arreglo de libreria reactstrap faltante

--- primerApp/my-app-test/src/components/NavBar.js
+++ primerApp/my-app-test/src/components/NavBar.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import { Navbar, NavbarBrand, NavItem, NavLink, Nav} from 'reactstrap';
+import React, { useState } from 'react'
+import { Navbar, NavbarBrand, NavItem, NavLink, Nav, NavbarToggler, Collapse, Tooltip} from 'reactstrap';
+import { BsFillPersonFill, BsSearch } from "react-icons/bs";
 
 const NavBar = () => {
+    const [collapsed, setCollapsed] = useState(true);
+    const toggleNavbar = () => setCollapsed(!collapsed);
+    
   return (
     <div>
-        <Navbar
-            color="light"
-            expand="md"
-            light
-        >
-            <NavbarBrand href="/" className='logo'>
-                <img title='Title company' src={process.env.PUBLIC_URL + 'logo.png'} alt='Logo Company' />
-            </NavbarBrand>
-            <Nav
-                className="ml-auto"
-                navbar
-            >
-                <NavItem>
+      <Navbar color="faded" light expand="md">
+        <NavbarBrand href="/" className="mr-auto logo">
+            <img title='Title company' src={process.env.PUBLIC_URL + 'logo.png'} alt='Logo Company' />
+        </NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse isOpen={!collapsed} navbar className=''>
+          <Nav navbar className='ms-auto mb-2 mb-lg-0'>
+          <NavItem>
                     <NavLink href="/">
                         About Us
                     </NavLink>
@@ -39,11 +38,17 @@ const NavBar = () => {
                 </NavItem>
                 <NavItem>
                     <NavLink href="/">
-                        Login
+                       <BsFillPersonFill title='Login'/>
                     </NavLink>
                 </NavItem>
-            </Nav>   
-        </Navbar>
+                <NavItem>
+                    <NavLink href="/">
+                        <BsSearch title='Search'/>
+                    </NavLink>
+                </NavItem>
+          </Nav>
+        </Collapse>        
+      </Navbar>
     </div>
   )
 }
